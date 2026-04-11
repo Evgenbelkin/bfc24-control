37c3e779fc93cdf88b3fb0044326cd3445a4b998
Evgenbelkin <sborkakabin@mail.ru>
2026-04-11 21:05:55 +0300

    fix(server): increase body size limit for image upload

--- server/index.js
+++ server/index.js
@@ -63,8 +63,8 @@ app.use(
   })
 );
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 // статика
 app.use("/uploads", express.static(UPLOADS_DIR));
