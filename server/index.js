b91037a95703566aebc5691dfddf961de15208df
Evgenbelkin <sborkakabin@mail.ru>
2026-04-14 12:57:56 +0300

    fix: box_qty edit + receiving boxes mode fix

--- server/index.js
+++ server/index.js
@@ -63,8 +63,8 @@ app.use(
   })
 );
 
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 // статика
 app.use("/uploads", express.static(UPLOADS_DIR));
